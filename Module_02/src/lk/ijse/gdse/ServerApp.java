4010af5175737859b1cceecfce297c7783b70aff
dimanranawaka <dimanranawaka@gmail.com>
2023-07-09 21:05:11 +0530

    Implementing Module_02

--- Module_02/src/lk/ijse/gdse/ServerApp.java
+++ Module_02/src/lk/ijse/gdse/ServerApp.java
@@ -7,23 +7,47 @@ import java.net.Socket;
 public class ServerApp {
     public static void main(String[] args) throws IOException {
 
+        // Define port number that the server will listen on
         final int PORT = 3000;
 
-        ServerSocket serverSocket = new ServerSocket(PORT); // Server Socket For listening
+        // Create a server socket on the defined port
+        ServerSocket serverSocket = new ServerSocket(PORT);
 
+        // Output to console indicating that the server is running
         System.out.println("Server is running in port : "+PORT);
 
-        Socket localSocket = serverSocket.accept(); // For accepting client's socket to keep the program running
+        // The server waits for a client connection using accept(). Program execution will pause here until a connection is made
+        Socket localSocket = serverSocket.accept();
         System.out.println("Client Accepted!");
 
-        DataOutputStream dataOutputStream = new DataOutputStream(localSocket.getOutputStream()); // to Send Messages
+        // Output stream - used for sending data to the client
+        DataOutputStream dataOutputStream = new DataOutputStream(localSocket.getOutputStream());
 
-        DataInputStream dataInputStream = new DataInputStream(localSocket.getInputStream()); // to Receive Messages
+        // Input stream - used for receiving data from the client
+        DataInputStream dataInputStream = new DataInputStream(localSocket.getInputStream());
 
-//        InputStreamReader inputStreamReader = new InputStreamReader(System.in); // Taking Input for send messages
+        // Reader for reading input from the user in the server-side console
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in)); //Converting Input data to String
+        // Variables to store the incoming message from the client and the reply from the server
+        String message = " ", reply = "";
 
+        // Server starts a loop that runs until the client sends a "finish" message
+        while (!message.equals("finish")){
 
+            // The server reads a message from the client
+            message = dataInputStream.readUTF();
+            // Print the client's message to the console
+            System.out.println(message);
+
+            // The server reads a reply from the console
+            reply = bufferedReader.readLine();
+
+            // The server sends the reply to the client
+            dataOutputStream.writeUTF(reply);
+
+            // Flushes the output stream, effectively sending any buffered output bytes to the client
+            dataOutputStream.flush();
+        }
     }
 }
