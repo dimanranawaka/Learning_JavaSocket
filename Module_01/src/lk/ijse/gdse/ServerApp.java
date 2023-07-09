fabc933eeb23436843451a4107a8ec426cc9dbf3
dimanranawaka <dimanranawaka@gmail.com>
2023-07-09 13:27:38 +0530

    Adding ServerSocket

---
+++ Module_01/src/lk/ijse/gdse/ServerApp.java
@@ -0,0 +1,10 @@
+package lk.ijse.gdse;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerApp {
+    public static void main(String[] args) {
+        ServerSocket serverSocket = new ServerSocket();
+    }
+}
