a9c59556b5c4784cb75f2a46d0b8d51a0bc777aa
dimanranawaka <dimanranawaka@gmail.com>
2023-07-09 13:33:31 +0530

    Configuring ServerApp

--- Module_01/src/lk/ijse/gdse/ServerApp.java
+++ Module_01/src/lk/ijse/gdse/ServerApp.java
@@ -1,10 +1,14 @@
 package lk.ijse.gdse;
 
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class ServerApp {
-    public static void main(String[] args) {
-        ServerSocket serverSocket = new ServerSocket();
+    public static void main(String[] args) throws IOException {
+        final int PORT = 8000;
+        ServerSocket serverSocket = new ServerSocket(8000); //Server Socket for listening
+
+        System.out.println("Server is running in port"+ PORT);
     }
 }
