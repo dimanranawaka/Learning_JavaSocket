71b4ba9266a58d6efc1c334c00a6431c69c109d4
dimanranawaka <dimanranawaka@gmail.com>
2023-07-09 13:57:18 +0530

    Configuring ClientApp

---
+++ Module_01/src/lk/ijse/gdse/ClientApp.java
@@ -0,0 +1,9 @@
+package lk.ijse.gdse;
+
+import java.net.Socket;
+
+public class ClientApp {
+    public static void main(String[] args) {
+        Socket socket = new Socket();// Client socket for request from Server
+    }
+}
