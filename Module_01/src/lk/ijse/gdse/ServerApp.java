5fafb8d6c0ce9cae69d306ad415c05405750e4a0
dimanranawaka <dimanranawaka@gmail.com>
2023-07-09 13:47:15 +0530

    Configuring ServerApp

--- Module_01/src/lk/ijse/gdse/ServerApp.java
+++ Module_01/src/lk/ijse/gdse/ServerApp.java
@@ -6,6 +6,7 @@ import java.net.Socket;
 
 public class ServerApp {
     public static void main(String[] args) throws IOException {
+
         final int PORT = 8000;
         ServerSocket serverSocket = new ServerSocket(8000); //Server Socket for listening
 
@@ -13,5 +14,6 @@ public class ServerApp {
 
         Socket localSocket = serverSocket.accept();// For accepting socket to Keep the program running
 
+        System.out.println("Port "+ localSocket.getPort()); //Checking client's port AKA Remote Port
     }
 }
