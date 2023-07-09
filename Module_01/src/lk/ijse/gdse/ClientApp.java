a5fd048c52e8f2d0f0660ddee998d0ddc51ab544
dimanranawaka <dimanranawaka@gmail.com>
2023-07-09 13:59:52 +0530

    Configuring ClientApp

--- Module_01/src/lk/ijse/gdse/ClientApp.java
+++ Module_01/src/lk/ijse/gdse/ClientApp.java
@@ -1,9 +1,11 @@
 package lk.ijse.gdse;
 
+import java.io.IOException;
 import java.net.Socket;
 
 public class ClientApp {
-    public static void main(String[] args) {
-        Socket socket = new Socket();// Client socket for request from Server
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("localhost",8000);// Client socket to request from Server
+
     }
 }
